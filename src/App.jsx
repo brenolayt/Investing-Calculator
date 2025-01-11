6b4eda1aba78b396240fdd1e21d0d47426a89a63
Breno Laytynher <brenolaytynher@outlook.com>
2025-01-10 21:42:39 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-      <p className='text-2xl'>Teste</p>
+      <menu className='grid place-items-center bg-gradient-to-tr from-gray-900 to-stone-800 w-screen h-screen'>
+        <h1 className='inline-block bg-gradient-to-r from-amber-300 via-amber-500 to-yellow-600 drop-shadow-2xl font-bold text-7xl text-transparent bg-clip-text'>
+          Investment Calculator
+        </h1>
+      </menu>
     </>
   )
 }
